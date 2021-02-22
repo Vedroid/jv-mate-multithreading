24532ea175d78109445edb768f1130072c8d6473
Vadim <Vedroidua@gmail.com>
2021-02-22 15:27:25 +0200

    Completed task

---
+++ src/main/java/ua/vedroid/multithreading/Counter.java
@@ -0,0 +1,13 @@
+package ua.vedroid.multithreading;
+
+public class Counter {
+    private int num = 0;
+
+    public void incrementNum() {
+        num++;
+    }
+
+    public int getNum() {
+        return num;
+    }
+}
---
+++ src/main/java/ua/vedroid/multithreading/ThreadB.java
@@ -0,0 +1,24 @@
+package ua.vedroid.multithreading;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ThreadB implements Runnable {
+    private static final Logger log = LogManager.getLogger();
+    private static final int MAX = 100;
+    private final Counter counter;
+
+    public ThreadB(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        log.info(threadName + " is running...");
+        while (counter.getNum() < MAX) {
+            counter.incrementNum();
+            log.info(threadName + " - " + counter.getNum());
+        }
+    }
+}
